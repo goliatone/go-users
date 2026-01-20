0d4d52eab81f1159592bf10bf3e3887059128dd1
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-19 23:44:44 -0800

    fix: use sub in migration

--- migrations/init.go
+++ migrations/init.go
@@ -1,9 +1,15 @@
 package migrations
 
 import (
+	"io/fs"
+
 	users "github.com/goliatone/go-users"
 )
 
 func init() {
-	Register(users.GetCoreMigrationsFS())
+	coreFS, err := fs.Sub(users.GetCoreMigrationsFS(), "data/sql/migrations")
+	if err != nil {
+		return
+	}
+	Register(coreFS)
 }
